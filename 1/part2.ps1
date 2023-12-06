5cf6a16957f94b86873181421d44fca735f938c3
tmeanor@starbucks.com <tmeanor@starbucks.com>
2023-12-06 12:51:05 -0800

    Update part2.ps1
    changed \d+ to \d  - this was left over from tshooting the isssue  but caused more issues than it should have

--- 1/part2.ps1
+++ 1/part2.ps1
@@ -1,24 +1,25 @@
 
-param ([string]$inputFile = "$psscriptroot\sampledata.txt")
+param ([string]$inputFile = "$psscriptroot\calibration.txt")
 $inputcontent = Get-Content $inputFile
 
 $sum_total_of_calibration_values = 0 
 
 $wordNumbers = @('zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine')
 
-$pattern = "(\d+|$($wordNumbers -join "|"))"
+$pattern = "(\d|$($wordNumbers -join "|"))"
 
 foreach ($line in $inputcontent){
     $digits = @()
     $selectMatches = $line  | Select-String -pattern $pattern -AllMatches
 
     $selectMatches.matches.value | ForEach-Object { 
-        $digits += if ($_ -match "\d+") { $_ } else { $wordNumbers.IndexOf($_) }
+        $digits += if ($_ -match "\d") { $_ } else { $wordNumbers.IndexOf($_) }
     }
 
-    $sum_total_of_calibration_values  += [int]("{0}{1}" -f $digits[0,-1])
+    $firstLast = [int]("{0}{1}" -f $digits[0,-1])
+    $sum_total_of_calibration_values += $firstLast
 
-    write-host "$($inputcontent.indexof($line)) $line $( $selectMatches.matches.value  -join ",") $( $digits -join ",") $([int]("{0}{1}" -f $digits[0,-1]))"
+    write-host "$($inputcontent.indexof($line)) $line $( $selectMatches.matches.value -join ",") $( $digits -join ",") $firstLast"
 }
 
 # the sample data calcs correctly but somehow the larger input does not
