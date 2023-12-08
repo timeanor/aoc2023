93cd8df12821aea1d7236a4aa2bd568592cccb45
tmeanor@starbucks.com <tmeanor@starbucks.com>
2023-12-08 14:16:02 -0800

    Update partNumbers.ps1

--- 3/partNumbers.ps1
+++ 3/partNumbers.ps1
@@ -1,89 +1,97 @@
 
 
-
-class partnumber {
-    [object]$cells = @()
+class symbol {
+    [string]$char
+    [Coord]$coord
+}
+class PartNumber {
+    [coord[]]$coords
     [int]$value
-    [bool]$isPartNumber
-    [bool]$isGear
-
+    [bool]$cogged
 }
-class cog {
-    [object]$cells = @()
+class Coord{
+    [int]$y
+    [int]$x
+    Coord ([int]$Y, [int]$X){
+        $this.y = $Y
+        $this.x = $X
+    }
 }
-
-function resolve-vector($x1,$y1,$x2,$y2){
-    
-     
+function resolve-vector($A,$B){
+    return ($B[1]-$A[1]),($B[0]-$A[0])     
 }
 
 $inputfiles =  "$psscriptroot\sampledata.txt"#, "$psscriptroot\inputData.txt"
-
+$inputfiles = 'C:\Users\tmeanor\source\repos\aoc2023\3\sampledata.txt'
 foreach ( $inputfile in $inputfiles ) {
     $partNumbers = @()
-    $cogs = @()
+    $symbols = @()
     $matrix = @()
     $content = Get-Content $inputfile
-    $matrix += ".$("." * ($content[0].Length))."
+    $matrix += ,".$("." * ($content[0].Length)).".ToCharArray()
     $content | ForEach-Object { $matrix += ,((".$_.").ToCharArray()) }
-    $matrix += ".$("." * ($content[0].Length))."
+    $matrix += ,".$("." * ($content[0].Length)).".ToCharArray()
 
-    for ( [int]$i = 1; $i -lt $matrix.GetUpperBound(0); $i++ ){   
-        for ( [int]$j = 1; $j -lt $matrix[1].Count; $j++ ){
-            if ($matrix[$i][$j] -match "\*"){ $cogs += @($i,$j) }
-            if ($matrix[$i][$j] -match "\d"){   
-                $partnumber = [partnumber]::new()              
+    # populate lists of all symbols and contiguous numbers 
+    for ( [int]$i = 1; $i -lt $matrix.Length -1; $i++ )
+    {   
+        for ( [int]$j = 1; $j -lt $matrix[0].Length -1; $j++ )
+        {
+            if ( $matrix[$i][$j] -match "[^\d|^\.]" ){ 
+                $symbol = [symbol]::new()  
+                $symbol.char = $matrix[$i][$j]
+                $symbol.coord = [Coord]::new($i,$j)
+                $symbols += $symbol
+            }
+            $chars = ''
+            if ( $matrix[$i][$j] -match "\d" ){   
+                $partnumber = [partnumber]::new()
                 while ($matrix[$i][$j] -match "\d"){
-                    $partnumber.cells += @($i, $j)  # transposed as y,x
+                    $chars += $matrix[$i][$j]
+                    $partnumber.coords += ,[Coord]::new($i,$j) 
                     $j++
-                }                
+                }     
+                $partnumber.value = [int]$chars          
                 $partNumbers += $partnumber
             } 
         } 
-    }
-return 
+    }   
     $partNumber_Sum = 0 
-    foreach ( $part in $partNumbers ){
-        
-        foreach ($cell in $part.cells){
-            $y,$x = $cell[0,1]
-            $part.value += [int]($matrix[$y][$x])
-        }           
-        foreach ($blah in $cogs){
-            $s = [math]::max($s - 1, 0 )
-            $e = [math]::min($e + 2, $matrix[0].Length -1 )
-                
-            for ($k = $p.line -1; $k -lt $part.line + 2; $k++ ){
-                for ($l = $s; $l -lt $e; $l++){
-                    $part.isPartNumber = $part.isPartNumber -or ($matrix[$k][$l] -match "[^\d|^\.]")
-                    $part.isGear = $part.isGear -or ($matrix[$k][$l] -match "\*") 
-                    if ( $part.isGear ){ $part.gear = ($k, $l) }
+   
+    $CoggedParts = @()
+
+    
+    $gears = $symbols | Where-Object { $_.char -eq "*" } 
+    $offsets = @{
+        Vertical = @( @(-1,0), @(1,0))
+        Left     = @(@(-1,-1), @(-1,0), @(-1,1))
+        Right    = @( @(1,-1),  @(1,0), @(1,1))
+    }
+    foreach ( $part in $partNumbers[0] )
+    {  
+        foreach ($coord in $part.coords)
+        {    
+            $coord 
+            foreach ($vert in $offsets.Vertical) {             
+                $offset = [Coord]::new(($coord.y + $vert[0]), ($coord.X + $vert[1]))
+                $offset 
+                $matrix[$offset.y][$offset.x]
+                $gears | where-object {
+                    ($_.coord.y -eq $offset.y) -and ($_.coord.x -eq $offset.x)
                 }
-            }
-        }           
+            }            
+        }
     }
 
-    $partNumbers | Where-Object { $_.isPartNumber } | ForEach-Object { $partNumber_Sum += $_.value }
+    $partNumbers | FT
+    $partNumbers | Where-Object { $_.isPartNumber } | ForEach-Object {         
+        $partNumber_Sum += $_.value 
+    }
     $gears =  $partNumbers | Where-Object { $_.isGear }
     #part 2
     $ratioSum = 0
     # $prevGear = $gears[0]
     $gearRatio = 1
-    # foreach ( $gear in ( $gears[1..-1] ) ){
-    #     $X1eqY2 = $gear.gear[0] -eq $prevGear.gear[0]
-    #     $X1eqX2 = $gear.gear[0] -eq $prevGear.gear[1]
-    #     $Y1eqY2 = $gear.gear[1] -eq $prevGear.gear[0]
-    #     $Y1eqx2 = $gear.gear[1] -eq $prevGear.gear[1]
-       
-    #     if ($X1eqY2 -or $X1eqX2 -or $Y1eqY2 -or $Y1eqx2) {
-    #         $gearRatio *= $gear.value * $prevGear.value
-    #         if ( ){
-    #             $ratioSum += $gearRatio 
-    #             $gearRatio = 1;
-    #         }
-    #     }   
-    #     $prevGear = $gear
-    # }
 
     [pscustomobject]@{
         Input = ($inputfile -split "\\")[-1]
